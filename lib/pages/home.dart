535a106cab8fe9829fe1ad554553cef8e7f444dd
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-03-31 21:11:13 +0530

    add image in assets folder

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,14 +12,106 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        color: Color(0xffedf3f6),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              decoration: BoxDecoration(color: Color(0xFF2a2d31)),
-              child: Row(
-                children: [],
+            Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 20.0, top: 50.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF2a2b31),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20.0),
+                      bottomRight: Radius.circular(20.0),
+                    ),
+                  ),
+                  height: 220.0,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Image.asset(
+                          "assets/photos/man.jpg",
+                          width: 50,
+                          height: 50,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 40.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Text(
+                          "Jacks Praro",
+                          style: TextStyle(
+                              fontSize: 22.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.only(top: 120.0, left: 20.0, right: 20.0),
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          bottomLeft: Radius.circular(20),
+                        ),
+                        child: Image.asset("assets/photos/qus.jpg",
+                            height: 210.0, width: 160, fit: BoxFit.cover),
+                      ),
+                      SizedBox(
+                        width: 27,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Play &\n Win",
+                            style: TextStyle(
+                                fontSize: 34.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Play Quiz guessing\nthe image",
+                            style: TextStyle(
+                                fontSize: 16.0,
+                                color: Color.fromARGB(249, 121, 121, 114),
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Text(
+                "Top Quiz Categories",
+                style: TextStyle(
+                    fontSize: 28.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
               ),
-            )
+            ),
           ],
         ),
       ),
