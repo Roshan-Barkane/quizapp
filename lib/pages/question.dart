6b571191d98cee563fcdce7d8875a197d5b69cb0
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-04-01 13:45:53 +0530

    add firebase in quiz app

--- lib/pages/question.dart
+++ lib/pages/question.dart
@@ -43,27 +43,119 @@ class _QuestionState extends State<Question> {
                 ],
               ),
             ),
-            Container(
-              // height: MediaQuery.of(context).size.height / 1.15,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(32.0),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: Container(
+                // height: MediaQuery.of(context).size.height / 1.15,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(32.0),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 30.0, left: 20.0, right: 20.0, bottom: 20.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset(
+                          "assets/photos/QutbMinar.jpg",
+                          height: 300.0,
+                          width: MediaQuery.of(context).size.width,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          right: 20.0, left: 20.0, bottom: 20.0),
+                      child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          border:
+                              Border.all(color: Color(0xFF818181), width: 2.0),
+                        ),
+                        child: Text(
+                          "Cristiano Ronaldo",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          right: 20.0, left: 20.0, bottom: 20.0),
+                      child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          border:
+                              Border.all(color: Color(0xFF818181), width: 2.0),
+                        ),
+                        child: Text(
+                          "Lionel Messi",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          right: 20.0, left: 20.0, bottom: 20.0),
+                      child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          border:
+                              Border.all(color: Color(0xFF818181), width: 2.0),
+                        ),
+                        child: Text(
+                          "Neymar",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          right: 20.0, left: 20.0, bottom: 20.0),
+                      child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          border:
+                              Border.all(color: Color(0xFF818181), width: 2.0),
+                        ),
+                        child: Text(
+                          "Andres Iniesta",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-              child: Column(
-                children: [
-                  Image.asset(
-                    "assets/photos/QutbMinar.jpg",
-                    height: 300.0,
-                    width: MediaQuery.of(context).size.width,
-                    fit: BoxFit.cover,
-                  )
-                ],
-              ),
-            )
+            ),
           ],
         ),
       ),
